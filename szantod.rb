6aaab728ceda799d1ed496e7596e3697dfef338e
Adam Gyulavari <adam.gyulavari@lab.coop>
2016-06-01 13:50:48 +0200

    add recapthca to form

--- szantod.rb
+++ szantod.rb
@@ -1,17 +1,29 @@
 # sincapun.rb
 require 'pony'
+require 'net/http'
+require 'uri'
+require 'json'
 
 class Szantod < Sinatra::Base
   get '/' do
     haml :index
   end
 
-  post '/contact' do
-    Pony.mail to: 'azeroths@gmail.com',
-              from: "#{params[:name]} <#{params[:email]}>",
-              subject: params[:message][0..40],
-              body: params[:message]
-    @sent = true
+  post '/' do
+    uri = URI.parse('https://www.google.com/recaptcha/api/siteverify')
+    response = Net::HTTP.post_form(uri, {
+      secret: '6LfWeiETAAAAALRKMOPFflJeuGabk0StvGoADdii',
+      response: params['g-recaptcha-response']
+      })
+    if JSON.parse(response.body)['success']
+      Pony.mail to: 'azeroths@gmail.com',
+                from: "#{params[:name]} <#{params[:email]}>",
+                subject: params[:message][0..40],
+                body: params[:message]
+      @sent = true
+    else
+      @error = true
+    end
     haml :index
   end
 end
